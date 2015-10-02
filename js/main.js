c0e9fe9d73eaa83d9bb1a9b0d4e1a7c2b4cea30b
unknown <Pledesma@BOGC100DIS05L.copaair.com>
2015-10-02 12:31:24 -0500

    falta definit el footer y el selector de idiomas

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
-	var contenido = $("#content"),
-		list_group = $("#list-group");
-
-	list_group.delegate('a', 'click', function(e){
+	var contenido = $("#content");
+	$("ul.nav-second-level").delegate('a', 'click', function(e){
 		var href = $(this).attr('href');
 		contenido.load( href + ' #blog-post' );
 		e.preventDefault();
