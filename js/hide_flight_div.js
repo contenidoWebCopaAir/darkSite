658a1df60b0701a05a85f708f4958fc51e2ff895
Pablo Ledesma Ortiz (CM) <Pledesma@copaair.com>
2015-12-21 15:00:54 -0500

    se agregó el script para el home

--- js/hide_flight_div.js
+++ js/hide_flight_div.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
 	var reserve = $("#subpanel_reserve_flights"),
 		tabFlights = $("#tab_flights"),
-		tabReserve = $("#tab_reserve");
+		tabReserve = $("#tab_reserve"),
+		widgetPromos = $("#widget_promos");
 
 	reserve.hide();
+	widgetPromos.hide();
 
 	tabFlights.removeClass('selected');
 	tabReserve.removeClass('selected');
 
 	tabFlights.click(function(){
-		reserve.slideToggle();
+		reserve.show();
 	});
 
 	tabReserve.click(function(){
-		reserve.slideToggle();
+		reserve.show();
 	});
 
 });
